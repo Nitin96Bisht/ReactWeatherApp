fa0e5a55751c9727f47e4d02b9029ba688f0d0f0
Nitin96Bisht <nitin.bisht96@gmail.com>
2017-08-23 20:08:13 +0530

    Initial commit

---
+++ example-promise.js
@@ -0,0 +1,55 @@
+// function getTempCallback (location, callback) {
+// 	callback(undefined, 78);
+// 	callback('City not found');
+// }
+
+// getTempCallback('Philadelphia', function (err, temp) {
+// 	if (err) {
+// 		console.log('error', err);
+// 	}
+// 	else {
+// 		console.log('success', temp)
+// 	}
+// });
+
+// function getTempPromise (location) {
+// 	return new Promise(function (resolve, reject) {
+// 		setTimeout(function () {
+// 			resolve(79);
+// 			reject('City not found');
+// 		}, 1000);
+// 	});
+// }
+
+// getTempPromise('Philadelphia').then(function (temp) {
+// 	console.log('promise success', temp);
+// }, function (err) {
+// 	console.log('promise error', err)
+// });
+
+
+//challenge
+function addPromise (a, b) {
+	return new Promise(function (resolve, reject) {
+		if (typeof a === 'number' && typeof b === 'number') {
+			resolve(a+b);
+		}
+		else {
+			reject('A and B need to be number');
+		}
+	});
+}
+
+
+addPromise(2, 3).then(function (sum) {
+	console.log('success', sum);
+}, function (err) {
+ 	console.log('error', err);
+});
+
+
+addPromise('nitin', 3).then(function (sum) {
+	console.log('this should not show up');
+}, function (err) {
+ 	console.log('this should appear', err);
+});
