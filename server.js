62774646f8f4a83accca94e5e97ec214221f2a8f
Nitin96Bisht <nitin.bisht96@gmail.com>
2017-08-25 12:11:46 +0530

    Update middleware for localhost

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-	if (req.header['x-forwarded-proto'] === 'http') {
-		next();
+	if (req.header['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
 	}
 	else {
-		res.redirect('http://' + req.hostname + req.url);
+		next();
 	}
 });
 
