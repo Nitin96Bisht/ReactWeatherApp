23265dd1501966bb76aeb420d6eed4c6ac74c101
Nitin96Bisht <nitin.bisht96@gmail.com>
2017-08-26 16:24:37 +0530

    Bug fixed

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,8 +5,7 @@ var About = (props) => {
     <div>
       <h1 className="text-center page-title">About</h1>
       <p>
-        This is a weather application build on React. I have built this
-        for The Complete React Web App Developer Course.
+        This is a weather application build on React.
       </p>
       <p>
         Here are some of the tools I used:
